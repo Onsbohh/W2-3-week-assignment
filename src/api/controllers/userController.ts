e76ee040895449acc914df9aa48eaabcd611d941
Onsbohh <onni.pajula@metropolia.fi>
2024-02-08 15:12:57 +0200

    Changes made

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -5,3 +5,45 @@
 // - userPutCurrent - update current user
 // - userDeleteCurrent - delete current user
 // - checkToken - check if current user token is valid: return data from res.locals.user as UserOutput. No need for database query
+import { Request, Response, NextFunction } from "express"
+import { User, UserOutput } from "../../types/DBTypes"
+import UserModel from "../models/userModel";
+import { MessageResponse } from "../../types/MessageTypes";
+import { userDeleteCat } from "../../../test/catFunctions";
+
+const userGet = async (
+    req: Request<{id: String}>,
+    res: Response<UserOutput>,
+    next: NextFunction
+) => {
+    try {
+        const user = await UserModel.findById(req.params.id);
+        if (!user) {
+            res.status(404);
+            console.log("User not found");
+            return;
+        }
+        res.json(user as UserOutput);
+    } catch (error) {
+        next(error);
+    }
+}; 
+
+const userListGet = async (
+    req: Request,
+    res: Response<UserOutput[]>,
+    next: NextFunction
+) => {
+    try {
+        const users = await UserModel.find();
+        if (!users) {
+            res.status(404);
+            console.log("No users found!");
+            return;
+        }
+        res.json(users);
+    } catch (error) {
+        next(error);
+    }
+};
+
